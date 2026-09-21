de17e39fea3d3411da80e2ac4a81209bebe10e30
EwenLh <ewenlhermitte@gmail.com>
2026-09-21 12:54:58 +0200

    mise à jour des test de user services

--- src/app/core/service/user.service.spec.ts
+++ src/app/core/service/user.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UserService } from './user.service';
+import { of, throwError } from 'rxjs';
+import { expect, describe, it, beforeEach, jest, beforeAll, afterAll } from '@jest/globals';
 import {provideHttpClient} from '@angular/common/http';
 
 describe('UserService', () => {
